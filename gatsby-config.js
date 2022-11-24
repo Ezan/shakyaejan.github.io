2e933a95824644b777cac8901155cb0919fa0a3d
Ejan Shakya <ezan.shakya@gmail.com>
2022-11-23 21:09:38 -0600

    initial

---
+++ gatsby-config.js
@@ -0,0 +1,54 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: `Ejan Shakya | Software Engineer`,
+    author: `Ejan Shakya`,
+    description: `A Software Engineer/Developer, a research enthusiast aiming to contribute in the field of paradigmatic Human-Computer Interaction, Artificial Intelligence and Machine Learning.`,
+    siteUrl: `https://ezan.github.io/profile/`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Ejan Shakya | Software Engineer`,
+        short_name: `CV`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#38B2AC`,
+        display: `standalone`,
+        icon: 'src/assets/site-icon.png'
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-transformer-json',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/data/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    'gatsby-plugin-netlify-cms',
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+        tailwind: true, // Enable tailwindcss support
+      }
+    }
+  ],
+};
